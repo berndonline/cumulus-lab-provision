49ebea0da5e18ad401ed686facdb88a72ba576e2
Bernd Lehmann <bernd@lehmann.be>
2017-11-19 01:29:21 +0100

    changed roles

--- production.sh
+++ production.sh
@@ -1,6 +1,12 @@
 vagrant up mgmt-1 mgmt-server --color
 exit_code=$?
 sleep 300
+vagrant up spine-1 spine-2 --color
+exit_code=$?
+sleep 60
+vagrant up edge-1 edge-2 --color
+exit_code=$?
+sleep 60
 vagrant up --color
 exit_code=$?
 sleep 600
