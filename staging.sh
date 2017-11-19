4c4053c3e49114f8d50b32d368dff4838bf38423
Bernd Lehmann <bernd@lehmann.be>
2017-11-19 21:56:35 +0100

    changed staging.sh

--- staging.sh
+++ staging.sh
@@ -1,11 +1,11 @@
-grant up mgmt-1 mgmt-server --color
+vgrant up mgmt-1 mgmt-server --color
 sleep 300
 vagrant up spine-1 spine-2 --color
 sleep 60
 vagrant up edge-1 edge-2 --color
 sleep 60
 vagrant up --color
-sleep 120
+sleep 300
 export ANSIBLE_FORCE_COLOR=true
 ansible-playbook ../site.yml
 exit_code=$?
